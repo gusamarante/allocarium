aaf4134c690477a9287161a955826beb67edc874
gamarante <gustavo.amarante@bwgi.com.br>
2025-07-24 10:58:56 -0300

    progress on max sharpe

--- allocarium/models/max_sharpe.py
+++ allocarium/models/max_sharpe.py
@@ -9,22 +9,24 @@ import pandas as pd
 import numpy as np
 
 
+# TODO Max Utility
+# TODO classes can have a ".from_ts" method to create the object from a time series of returns, this would require an establisehed estimator for mu and sigma
+
 class MaxSharpe:
 
     def __init__(self, mu, cov, rf=0, allow_shorts=True):
-        # TODO Documentation
-        # TODO Optimal risky porfolio (max sharpe / tangency)
+        # TODO Documentation (for when rf is available, returns the tangency portfolio)
 
-        # Assertions
-        self._assert_indexes(mu, cov)
+        self._assertions(mu, cov)
 
         # Save attributes
+        self.n_assets = mu.shape[0]
+        self.asset_names = list(mu.index)
         self.mu = mu
         self.cov = cov
-        self.sigma = pd.Series(data=np.sqrt(np.diag(cov)), index=mu.index)
+        self.std = pd.Series(data=np.sqrt(np.diag(cov)), index=mu.index)
         self.rf = rf
         self.allow_shorts = allow_shorts
-        self.n_assets = self._n_assets()
 
         if allow_shorts:
             # TODO Analytical solution of the max sharpe
@@ -32,6 +34,80 @@ class MaxSharpe:
         else:
             # Numerical solution
             self.mu_p, self.sigma_p, self.risky_weights, self.sharpe_p = self._numerical_tangency()
+
+    @staticmethod
+    def _assertions(mu, cov):
+        # TODO mu and cov types
+        cond1 = sorted(mu.index) == sorted(cov.index)
+        cond2 = sorted(cov.index) == sorted(cov.columns)
+        cond = cond1 and cond2
+        assert cond, "elements in the input indexes do not match"
+
+    def _numerical_tangency(self):
+        """
+        Used when short selling is not allowed, so there is no analytical
+        solution
+        """
+
+        if self.n_assets == 1:  # one risky asset (analytical)
+            mu_p = self.mu.iloc[0]
+            sigma_p = self.cov.iloc[0, 0]
+            sharpe_p = (mu_p - self.rf) / sigma_p
+            weights = pd.Series(
+                data={self.mu.index[0]: 1},
+                name='Risky Weights',
+            )
+
+        else:  # multiple risky assets (optimization)
+
+            # objective function (notice the sign change on the return value)
+            def sharpe(x):
+                return - self._sharpe(x, self.mu.values, self.cov.values, self.rf, self.n_assets)
+
+            # budget constraint
+            constraints = ({'type': 'eq',
+                            'fun': lambda w: w.sum() - 1})
+
+            # Create bounds for the weights if short-selling is restricted
+            bounds = Bounds(np.zeros(self.n_assets), np.ones(self.n_assets))
+
+            # initial guess
+            w0 = np.zeros(self.n_assets)
+            w0[0] = 1
+
+            # Run optimization
+            res = minimize(sharpe, w0,
+                           method='SLSQP',
+                           constraints=constraints,
+                           bounds=bounds,
+                           options={'ftol': 1e-9, 'disp': False})
+
+            if not res.success:
+                raise RuntimeError("Convergence Failed")
+
+            # Compute optimal portfolio parameters
+            mu_p = np.sum(res.x * self.mu.values.flatten())
+            sigma_p = np.sqrt(res.x @ self.cov @ res.x)
+            sharpe_p = - sharpe(res.x)
+            weights = pd.Series(
+                index=self.mu.index,
+                data=res.x,
+                name='Risky Weights',
+            )
+
+        return mu_p, sigma_p, weights, sharpe_p
+
+    @staticmethod
+    def _sharpe(w, mu, cov, rf, n):
+        er = np.sum(w * mu)
+
+        w = np.reshape(w, (n, 1))
+        risk = np.sqrt(w.T @ cov @ w)[0][0]
+
+        sharpe = (er - rf) / risk
+        return sharpe
+
+class MaxSharpeOLD:
 
     def plot(self,
              figsize=(10, 7),
@@ -183,83 +259,4 @@ class MaxSharpe:
 
         return mu_range, sigma_range
 
-    def _numerical_tangency(self):
-        """
-        Used when short selling is not allowed, so there is no analytical solution
-        """
 
-        if self.n_assets == 1:  # one risky asset (analytical)
-            mu_p = self.mu.iloc[0]
-            sigma_p = self.cov.iloc[0, 0]
-            sharpe_p = (mu_p - self.rf) / sigma_p
-            weights = pd.Series(
-                data={self.mu.index[0]: 1},
-                name='Risky Weights',
-            )
-
-        else:  # multiple risky assets (optimization)
-
-            # objective function (notice the sign change on the return value)
-            def sharpe(x):
-                return - self._sharpe(x, self.mu.values, self.cov.values, self.rf, self.n_assets)
-
-            # budget constraint
-            constraints = ({'type': 'eq',
-                            'fun': lambda w: w.sum() - 1})
-
-            # Create bounds for the weights if short-selling is restricted
-            bounds = Bounds(np.zeros(self.n_assets), np.ones(self.n_assets))
-
-            # initial guess
-            w0 = np.zeros(self.n_assets)
-            w0[0] = 1
-
-            # Run optimization
-            res = minimize(sharpe, w0,
-                           method='SLSQP',
-                           constraints=constraints,
-                           bounds=bounds,
-                           options={'ftol': 1e-9, 'disp': False})
-
-            if not res.success:
-                raise RuntimeError("Convergence Failed")
-
-            # Compute optimal portfolio parameters
-            mu_p = np.sum(res.x * self.mu.values.flatten())
-            sigma_p = np.sqrt(res.x @ self.cov @ res.x)
-            sharpe_p = - sharpe(res.x)
-            weights = pd.Series(index=self.mu.index,
-                                data=res.x,
-                                name='Risky Weights')
-
-        return mu_p, sigma_p, weights, sharpe_p
-
-    @staticmethod
-    def _assert_indexes(mu, cov):
-        cond = sorted(mu.index) == sorted(cov.index)
-        assert cond, "elements in the input indexes do not match"
-
-    def _n_assets(self):
-        """
-        Makes sure that all inputs have the correct shape and returns the number of assets
-        """
-        shape_mu = self.mu.shape
-        shape_sigma = self.cov.shape
-
-        max_shape = max(shape_mu[0], shape_sigma[0], shape_sigma[1])
-        min_shape = min(shape_mu[0], shape_sigma[0], shape_sigma[1])
-
-        if max_shape == min_shape:
-            return max_shape
-        else:
-            raise AssertionError('Mismatching dimensions of inputs')
-
-    @staticmethod
-    def _sharpe(w, mu, cov, rf, n):
-        er = np.sum(w * mu)
-
-        w = np.reshape(w, (n, 1))
-        risk = np.sqrt(w.T @ cov @ w)[0][0]
-
-        sharpe = (er - rf) / risk
-        return sharpe
--- examples/max_sharpe_dev.py
+++ examples/max_sharpe_dev.py
@@ -2,4 +2,3 @@ from allocarium.utils import mu_cov_example
 
 mu, cov = mu_cov_example()
 
-# TODO parei aqui
